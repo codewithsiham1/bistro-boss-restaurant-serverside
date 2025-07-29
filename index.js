df56443468e0b61609f456418f20acc21580caee
moudud <sihamislam18@gmail.com>
2025-07-29 09:58:50 +0600

    fix: added production CORS origin

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ const port=process.env.PORT||5000;
 // middleware
 // Correct CORS options
 const corsOption = {
-  origin: ['http://localhost:5173'],
+  origin: [
+    'http://localhost:5173', // for local dev
+    'https://resshop-a427b.web.app' // for deployed frontend
+  ],
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
 };
 app.use(cors(corsOption));
 app.use(express.json())
